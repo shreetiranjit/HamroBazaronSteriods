6fe6c08bfbb4837d024a793c9fb335b50c09c58e
shreetiranjit <ranjit.shreeti1@gmail.com>
2022-06-24 09:59:56 +0545

    ⚙ Store app initialized

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def store(request): 
+    context = {}
+    return render(request, 'store/store.html')
+
+def cart(request): 
+    context = {}
+    return render(request, 'store/cart.html') 
+
+def checkout(request): 
+    context = {}
+    return render(request, 'store/checkout.html')
