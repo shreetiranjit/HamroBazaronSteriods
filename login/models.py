9a7d30c201546960222fb63c4e3fd7e6cbfe055b
shreetiranjit <ranjit.shreeti1@gmail.com>
2022-06-25 13:17:02 +0545

    necessary models has been created but might update it later

--- login/models.py
+++ login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class userCredentials(models.Model):
+    fullname = models.CharField(max_length=32, null = False)
+    email = models.EmailField(max_length=80 , null =False)
+    password = models.CharField(max_length=32, null = False)
+    
+
+def __str__(self):
+    return self.title 
